f2d60f6257a2a7695af9ec04fee0c91aac6947c1
LordCadeu <gbcadeu@gmail.com>
2024-07-30 23:48:56 -0600

    Files CamWeb & Ultrasonic Sensor
    Contains Arduino code for Esp32-CAM & Ultrasonic Sensor HC-SR04

---
+++ CameraWebServerUltrasonico.ino
@@ -0,0 +1,297 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <PubSubClient.h> //Biblioteca para conexion MQTT
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+//            You must select partition scheme from the board menu that has at least 3MB APP space.
+//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15 
+//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
+
+// ===================
+// Select camera model
+// ===================
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+
+#include "camera_pins.h"
+
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+
+const char* ssid = "INFINITUM1B17";  // Aquí debes poner el nombre de tu red, en nuestro caso generamos una red local
+const char* password = "6hQgf7hRxj";  // Aquí debes poner la contraseña de tu red
+
+const char* mqtt_server = "192.168.1.134"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(192,168,1,134);
+void startCameraServer();
+void setupLedFlash(int pin);
+WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
+PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
+
+int flashLedPin = 4;  // Para indicar el estatus de conexion
+int statusLedPin = 33; // Para ser controlado por MQTT
+long timeNow, timeLast; // Variables de control de tiempo no bloqueante
+int data = 0; // Contador
+int wait = 500;  // Indica la espera cada 1 segundos para envio de mensajes MQTT
+int Trig=13, Echo=12, Led=3;
+int duracion,distancia, d;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    if(psramFound()){
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(7, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  //if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    //s->set_saturation(s, -2); // lower the saturation
+    s->set_saturation(s, 2);
+  //}
+  // drop down frame size for higher initial frame rate
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
+
+// Setup LED FLash if LED pin is defined in camera_pins.h
+#if defined(LED_GPIO_NUM)
+  setupLedFlash(LED_GPIO_NUM);
+#endif
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+  Serial.begin (115200);
+  Serial.println ("Programa iniciado");
+  //
+  pinMode (flashLedPin, OUTPUT);
+  pinMode (statusLedPin, OUTPUT);
+  digitalWrite (flashLedPin, LOW);
+  digitalWrite (statusLedPin, HIGH);
+
+  pinMode(Trig,OUTPUT);
+  pinMode(Echo,INPUT);
+  pinMode(Led,OUTPUT);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Conectar a ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password); // Esta es la función que realiz la conexión a WiFi
+ 
+  while (WiFi.status() != WL_CONNECTED) { // Este bucle espera a que se realice la conexión
+    digitalWrite (statusLedPin, HIGH);
+    delay(500); //dado que es de suma importancia esperar a la conexión, debe usarse espera bloqueante
+    digitalWrite (statusLedPin, LOW);
+    Serial.print(".");  // Indicador de progreso
+    delay (5);
+  }
+  
+  // Cuando se haya logrado la conexión, el programa avanzará, por lo tanto, puede informarse lo siguiente
+  Serial.println();
+  Serial.println("WiFi conectado");
+  Serial.println("Direccion IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Si se logro la conexión, encender led
+  if (WiFi.status () > 0){
+  digitalWrite (statusLedPin, LOW);
+  }
+   Serial.println("Linea 88, wifi estatus, se logro la conexion");
+  delay (1000); // Esta espera es solo una formalidad antes de iniciar la comunicación con el broker
+
+   Serial.println("Comienza la conexion al broquer");
+  // Conexión con el broker MQTT
+  client.setServer(server, 1883); // Conectarse a la IP del broker en el puerto indicado
+  //client.setCallback(callback); // Activar función de CallBack, permite recibir mensajes MQTT y ejecutar funciones a partir de ellos
+  delay(1500);  // Esta espera es preventiva, espera a la conexión para no perder información
+Serial.println("Termina la conexion al broquer");
+//  dht.begin(); // Función que inicia el sensor
+  
+  timeLast = millis (); // Inicia el control de tiempo
+
+  
+}
+
+void loop() {
+  // Do nothing. Everything is done in another task by the web server
+  delay(2000);
+  if (!client.connected()) {
+    reconnect();  // En caso de que no haya conexión, ejecutar la función de reconexión, definida despues del void setup ()
+    Serial.println("Se ha mandado a la funcion reconectar GMA");
+  }// fin del if (!client.connected())
+  client.loop(); // Esta función es muy importante, ejecuta de manera no bloqueante las funciones necesarias para la comunicación con el broker
+  delay(wait);
+  timeNow = millis(); // Control de tiempo para esperas no bloqueantes
+  if (timeNow - timeLast > wait) { // Manda un mensaje por MQTT cada cinco segundos
+    timeLast = timeNow; // Actualización de seguimiento de tiempo
+    // distancia:
+    digitalWrite(Trig,HIGH);
+    delay(1);
+    digitalWrite(Trig,LOW);
+    duracion=pulseIn(Echo,HIGH);
+    distancia=duracion/58.2;
+    if(distancia>400)
+    distancia=400;
+    Serial.println(distancia);
+    delay(200);
+    if(distancia<=30 && distancia>=0)
+    {
+      digitalWrite(Led,HIGH);
+      delay(distancia*5);
+      digitalWrite(Led,LOW);
+
+    }
+    
+    d=distancia;
+    // Secuencia que se asegura de que la conexión con el sensor exista
+    if (isnan(d)) {
+      Serial.println(F("Failed to read from Ultrasonic sensor!"));
+      return;
+    }
+    if(distancia<=30 && distancia>=0)
+    {
+    //printLocalTime();
+    String json = "{\"id\":\"Distancia\",\"dist\":"+String(d)+"}";
+    //String json = "{\"id\":\"Temperatura\",\"temp\":"+String(d)+",\"hum\":"+String(h)+"}";
+    Serial.println(json); // Se imprime en monitor solo para poder visualizar que el string esta correctamente creado
+    int str_len = json.length() + 1;//Se calcula la longitud del string
+    char char_array[str_len];//Se crea un arreglo de caracteres de dicha longitud
+    json.toCharArray(char_array, str_len);//Se convierte el string a char array    
+    client.publish("IoT/mqtt/CarritoObjects", char_array); 
+    }
+    //codigoIoT/flow5/mqtt/clima, o checar si es: codigoIoT/Mor/mqtt/flow4 (checar  *) 
+  }// fin del if (timeNow - timeLast > wait)
+
+  
+}
+void reconnect() {
+  // Bucle hasta lograr conexión
+  while (!client.connected()) { // Pregunta si hay conexión
+    Serial.print("Tratando de contectarse...");
+    // Intentar reconexión
+    if (client.connect("ESP32CAMClient")) { //Pregunta por el resultado del intento de conexión
+      Serial.println("Conectado");
+      //client.subscribe("codigoIoT/mqtt"); // Esta función realiza la suscripción al tema
+      client.subscribe("IoT/mqtt/CarritoObjects"); // Esta función realiza la suscripción al tema
+    }// fin del  if (client.connect("ESP32CAMClient"))
+    else {  //en caso de que la conexión no se logre
+      Serial.print("Conexion fallida, Error rc=");
+      Serial.print(client.state()); // Muestra el codigo de error
+      Serial.println(" Volviendo a intentar en 5 segundos");
+      // Espera de 5 segundos bloqueante
+      delay(1000);
+      Serial.println (client.connected ()); // Muestra estatus de conexión
+    }// fin del else
+  }// fin del bucle while (!client.connected())
+}// fin de void reconnect(
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
